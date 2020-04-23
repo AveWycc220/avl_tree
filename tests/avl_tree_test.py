c554bbfa3a370fe6609b78ab0015ed0f17f0eca4
AveWycc220 <mik16748@gmail.com>
2020-04-23 23:51:12 +0300

    Add all needed test for AVLTree : class.methods

--- tests/avl_tree_test.py
+++ tests/avl_tree_test.py
@@ -3,8 +3,6 @@ import sys
 import unittest
 sys.path.append('../')
 from modules.avl_tree import AVLTree
-#TODO _insert_node, _recompute_heights, _rebalance, _rrc, _rlc, _llc, _lrc, _search_for_delete,\
-#TODO _search_node, delete(, _remove_leaf, _swap_and_remove, _find_smallest, _swap_nodes, _remove_branch
 
 class TestAVLTreeMethods(unittest.TestCase):
     """ Class for test methods of tree """
@@ -61,7 +59,8 @@ class TestAVLTreeMethods(unittest.TestCase):
         self.assertEqual(tree_third.search("two"), True)
         self.assertEqual(tree_third.search("three"), True)
         self.assertEqual(tree_third._AVLTree__root.value, "two")
-    def test_search(self): 
+
+    def test_search(self):
         """ Test for search() """
         # First situation.
         tree = AVLTree()
@@ -78,3 +77,153 @@ class TestAVLTreeMethods(unittest.TestCase):
         tree_list = AVLTree()
         tree_list.insert([5, 4, 5])
         self.assertEqual(tree_list.search([5, 4, 5]), True)
+
+    def test_insert_node(self):
+        """ Test for _insert_node() """
+        tree = AVLTree()
+        tree.insert(5) # Set root
+        tree._insert_node(tree._AVLTree__root, 15)
+        self.assertEqual(tree.search(15), True)
+        self.assertEqual(tree.search(15, True), 'Node(Value: 15, Height: 0, Right: None, Parent: 5, Left: None)')
+
+    def test_recompute_heights(self):
+        """ Test for recompute_heights() """
+        tree = AVLTree()
+        tree.insert(5)
+        tree.insert(15)
+        tree.insert(2)
+        self.assertEqual(tree.search(15, True), 'Node(Value: 15, Height: 0, Right: None, Parent: 5, Left: None)')
+        self.assertEqual(tree.search(5, True), 'Node(Value: 5, Height: 1, Right: 15, Parent: None, Left: 2)')
+        self.assertEqual(tree.search(2, True), 'Node(Value: 2, Height: 0, Right: None, Parent: 5, Left: None)')
+    
+    def test_rebalance_rrc(self):
+        """ Test for rebalance and rotations. RRC """
+        tree_rrc = AVLTree()
+        tree_rrc.insert(5)
+        tree_rrc.insert(10)
+        tree_rrc.insert(15)
+        self.assertEqual(tree_rrc.search(5, True), 'Node(Value: 5, Height: 0, Right: None, Parent: 10, Left: None)')
+        self.assertEqual(tree_rrc.search(10, True), 'Node(Value: 10, Height: 1, Right: 15, Parent: None, Left: 5)')
+        self.assertEqual(tree_rrc.search(15, True), 'Node(Value: 15, Height: 0, Right: None, Parent: 10, Left: None)')
+
+    def test_rebalance_rlc(self):
+        """ Test for rebalance and rotations. RLC """
+        tree_rlc = AVLTree()
+        tree_rlc.insert(5)
+        tree_rlc.insert(10)
+        tree_rlc.insert(8)
+        self.assertEqual(tree_rlc.search(5, True), 'Node(Value: 5, Height: 0, Right: None, Parent: 8, Left: None)')
+        self.assertEqual(tree_rlc.search(10, True), 'Node(Value: 10, Height: 0, Right: None, Parent: 8, Left: None)')
+        self.assertEqual(tree_rlc.search(8, True), 'Node(Value: 8, Height: 1, Right: 10, Parent: None, Left: 5)')
+
+    def test_rebalance_llc(self):
+        """ Test for rebalance and rotations. LLC """
+        tree_llc = AVLTree()
+        tree_llc.insert(5)
+        tree_llc.insert(4)
+        tree_llc.insert(3)
+        self.assertEqual(tree_llc.search(5, True), 'Node(Value: 5, Height: 0, Right: None, Parent: 4, Left: None)')
+        self.assertEqual(tree_llc.search(4, True), 'Node(Value: 4, Height: 1, Right: 5, Parent: None, Left: 3)')
+        self.assertEqual(tree_llc.search(3, True), 'Node(Value: 3, Height: 0, Right: None, Parent: 4, Left: None)')
+
+    def test_rebalance_lrc(self): 
+        """ Test for rebalance and rotations. LRC """
+        tree_lrc = AVLTree()
+        tree_lrc.insert(10)
+        tree_lrc.insert(6)
+        tree_lrc.insert(8)
+        self.assertEqual(tree_lrc.search(10, True), 'Node(Value: 10, Height: 0, Right: None, Parent: 8, Left: None)')
+        self.assertEqual(tree_lrc.search(6, True), 'Node(Value: 6, Height: 0, Right: None, Parent: 8, Left: None)')
+        self.assertEqual(tree_lrc.search(8, True), 'Node(Value: 8, Height: 1, Right: 10, Parent: None, Left: 6)')
+
+    def test_search_for_delete(self):
+        """ Test for _search_for_delete(), that return node, unlike search() """
+        tree = AVLTree()
+        tree.insert(10)
+        tree.insert(7)
+        self.assertEqual(str(type(tree._search_for_delete(10))), "<class 'modules.node.Node'>")
+        self.assertEqual(str(type(tree._search_for_delete(6))), "<class 'NoneType'>")
+        tree_empty = AVLTree()
+        self.assertEqual(str(type(tree_empty._search_for_delete(6))), "<class 'NoneType'>")
+
+    def test_search_node(self):
+        """ Test for _search_node(), that search node by recursion in search() and _search_for_delete() """
+        tree = AVLTree()
+        tree.insert(5)
+        tree.insert(10)
+        self.assertEqual(tree._search_node(tree._AVLTree__root, 5), True)
+        self.assertEqual(str(tree._search_node(tree._AVLTree__root, 10, True)), 'Node(Value: 10, Height: 0, Right: None, Parent: 5, Left: None)')
+        self.assertEqual(tree._search_node(tree._AVLTree__root, 15), False)
+        self.assertEqual(tree._search_node(tree._AVLTree__root, 18, True), None)
+
+    def test_delete_leaf(self):
+        """ Test for delete(). First situation. _remove_leaf() """
+        tree_first = AVLTree()
+        tree_first.insert(5)
+        tree_first.insert(10)
+        tree_first.delete(10)
+        self.assertEqual(tree_first.search(10), False)
+        self.assertEqual(tree_first.search(5, True), 'Node(Value: 5, Height: 0, Right: None, Parent: None, Left: None)')
+
+    def test_delete_branch(self):
+        """ Test for delete(). Second situation. _remove_branch() """
+        tree_second = AVLTree()
+        tree_second.insert(5)
+        tree_second.insert(10)
+        tree_second.delete(5)
+        self.assertEqual(tree_second.search(5), False)
+        self.assertEqual(tree_second.search(10, True), 'Node(Value: 10, Height: 0, Right: None, Parent: None, Left: None)')
+
+    def test_delete_swap_and_remove(self): 
+        """" Test for delete(). Third situation. _swap_and_remove() """
+        tree_third = AVLTree()
+        tree_third.insert(5)
+        tree_third.insert(10)
+        tree_third.insert(2)
+        tree_third.delete(5)
+        self.assertEqual(tree_third.search(5), False)
+        self.assertEqual(tree_third.search(10, True), 'Node(Value: 10, Height: 1, Right: None, Parent: None, Left: 2)')
+        self.assertEqual(tree_third.search(2, True), 'Node(Value: 2, Height: 0, Right: None, Parent: 10, Left: None)')
+
+    def test_find_smallest(self):
+        """ Test for _find_smallest() """
+        tree = AVLTree()
+        tree.insert(2)
+        tree.insert(3)
+        tree.insert(1)
+        tree.insert(0)
+        self.assertEqual(str(tree._find_smallest(tree._AVLTree__root)), 'Node(Value: 0, Height: 0, Right: None, Parent: 1, Left: None)')
+
+    def test_swap_nodes(self):
+        """ Test for _swap_nodes() """
+        tree = AVLTree()
+        tree.insert(2)
+        tree.insert(5)
+        tree.insert(1)
+        self.assertEqual(str(tree.search(2, True)), 'Node(Value: 2, Height: 1, Right: 5, Parent: None, Left: 1)')
+        self.assertEqual(str(tree.search(5, True)), 'Node(Value: 5, Height: 0, Right: None, Parent: 2, Left: None)')
+        tree._swap_nodes(tree._search_node(tree._AVLTree__root, 2, True), tree._search_node(tree._AVLTree__root, 5, True))
+        self.assertEqual(str(tree.search(2, True)), 'None') # None, because 5 > 2 then search() cant fint this node
+        self.assertEqual(str(tree.search(5, True)), 'Node(Value: 5, Height: 1, Right: 2, Parent: None, Left: 1)')
+
+    def test_max(self):
+        """ Test for max() """
+        tree = AVLTree()
+        self.assertEqual(tree.max(True), None)
+        self.assertEqual(tree.max(), 'Tree is empty')
+        tree.insert(6)
+        tree.insert(2)
+        tree.insert(1616)
+        self.assertEqual(tree.max(True), 1616)
+        self.assertEqual(tree.max(), '1616')
+
+    def test_min(self):
+        """ Test for min() """
+        tree = AVLTree()
+        self.assertEqual(tree.min(True), None)
+        self.assertEqual(tree.min(), 'Tree is empty')
+        tree.insert(6)
+        tree.insert(2)
+        tree.insert(1616)
+        self.assertEqual(tree.min(True), 2)
+        self.assertEqual(tree.min(), '2')
